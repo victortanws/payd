d0e3b8740491bd89d257bb68e32bf17d1aa6fe43
Raghu Betina <raghu@firstdraft.com>
2017-03-16 03:45:34 +0000

    Validate presence of payment_informations.cardholder_name

--- app/models/payment_information.rb
+++ app/models/payment_information.rb
@@ -5,6 +5,8 @@ class PaymentInformation < ApplicationRecord
 
   # Validations
 
+  validates :cardholder_name, :presence => true
+
   validates :cvv, :numericality => true
 
   validates :cvv, :length => { :minimum => 3, :maximum => 3 }
