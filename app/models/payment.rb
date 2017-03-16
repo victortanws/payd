0ebd8e9c64c8b10d4032e33ebc1dcb7fb65e4167
Raghu Betina <raghu@firstdraft.com>
2017-03-16 03:45:34 +0000

    Direct association between payments and comments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,9 @@
 class Payment < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   belongs_to :creditordebtor,
              :class_name => "User",
              :foreign_key => "other_id"
