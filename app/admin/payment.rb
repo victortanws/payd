af3fc8d9b79f24e975227940ac3ff58fe66c24e8
Raghu Betina <raghu@firstdraft.com>
2017-03-16 03:45:56 +0000

    Whitelist columns in Payment for ActiveAdmin

--- app/admin/payment.rb
+++ app/admin/payment.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Payment do
 
+ permit_params :user_id, :other_id, :description, :amount_paid, :payment
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
